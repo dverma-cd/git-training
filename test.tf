962c409e35c272fbdf2d08be6bdd8299ec875af1
Deepak Verma <deepak.verma@clouddrove.com>
2026-03-05 15:45:39 +0530

    added code for .gitignore

---
+++ test.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  name        = "app"
+  environment = "test"
+  label_order = ["name", "environment", "location"]
+}
+
+##-----------------------------------------------------------------------------
+## Resource Group module call
+## Resource group in which all resources will be deployed.
+##-----------------------------------------------------------------------------
+module "resource_group" {
+  source      = "terraform-az-modules/resource-group/azurerm"
+  version     = "1.0.3"
+  name        = local.name
+  environment = local.environment
+  label_order = local.label_order
+  location    = "northeurope"
+}
